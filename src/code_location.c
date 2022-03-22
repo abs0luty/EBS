5b778f5f488c9edc645fc3c32b7078a653da4022
Salimgereyev Adi <adisalimgereev@gmail.com>
2022-03-22 15:04:00 +0600

    Refactoring

--- src/code_location.c
+++ src/code_location.c
@@ -3,22 +3,22 @@
 /// Copyright (c) 2022 Salimgereyev Adi
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
+/// of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
 ///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
 
 #include "code_location.h"
 
@@ -38,7 +38,8 @@ struct code_location *copy_code_location(const struct code_location *location)
 				 location->column);
 }
 
-struct code_location *copy_advanced_code_location(const struct code_location *location)
+struct code_location *copy_advanced_code_location(
+	const struct code_location *location)
 {
 	return new_code_location(location->index + 1, location->line,
 				 location->column + 1);
--- src/code_location.h
+++ src/code_location.h
@@ -3,22 +3,22 @@
 /// Copyright (c) 2022 Salimgereyev Adi
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
+/// of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
 ///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
 
 #ifndef _code_location_h_
 #define _code_location_h_
@@ -40,7 +40,8 @@ struct code_location *copy_code_location(const struct code_location *location);
 
 // can be used for tokenizing punctuators where we need to get
 // a location of the next character
-struct code_location *copy_advanced_code_location(const struct code_location *location);
+struct code_location *copy_advanced_code_location(
+	const struct code_location *location);
 
 char *dump_code_location(const struct code_location *location);
 
